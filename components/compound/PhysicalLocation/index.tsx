5c24b3cfadb74db50e58b312556f49272d8af513
Oyindamola Akogun <oyindamolaakogun@Oyindamolas-MacBook-Pro.local>
2024-04-07 11:17:41 +0100

    chore: added video section

--- components/compound/PhysicalLocation/index.tsx
+++ components/compound/PhysicalLocation/index.tsx
@@ -4,13 +4,27 @@ import styles from "../../../styles/Physicallocation.module.scss";
 
 const index = () => {
   return (
-    <section className={`${styles.physical_hero} flex items-end w-full`}>
-      <MoreInfo
-        buttonId={3}
-        headingCustomClass="text-white font-[700] text-[22px] mb-4"
-        buttonTitle="DISCOVER MORE"
-        heading="SHOP PHYSICAL LOCATIONS"
-      />
+    <section className={`flex items-end justify-center w-full`}>
+      <video
+        loop
+        autoPlay={true}
+        className="w-full h-screen object-cover object-center z-[-1]"
+        playsInline
+        muted
+      >
+        <source src="/videos/zara-video.mp4" type="video/mp4" />
+        <source src="/videos/zara-video.webm" type="video/webm" />
+        <track kind="captions" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="absolute w-fit mx-auto -translate-y-[20px]">
+        <MoreInfo
+          buttonId={3}
+          headingCustomClass="text-white font-[700] text-[22px] mb-4"
+          buttonTitle="DISCOVER MORE"
+          heading="SHOP PHYSICAL LOCATIONS"
+        />
+      </div>
     </section>
   );
 };
