9fc4758c5ae10e8010d85fafadc393817a6d6a85
Oyindamola Akogun <oyindamolaakogun@Oyindamolas-MacBook-Pro.local>
2024-04-07 17:40:26 +0100

    feature

--- components/compound/PhysicalLocation/index.tsx
+++ components/compound/PhysicalLocation/index.tsx
@@ -12,14 +12,14 @@ const Index = () => {
         className="w-full md:h-screen h-[90vh] object-cover object-center"
         playsInline
         muted
-        onClick={(e) => {
+       /*  onClick={(e) => {
           e.preventDefault();
           if (e.currentTarget.paused) {
             e.currentTarget.play();
           } else {
             e.currentTarget.pause();
           }
-        }}
+        }} */
       >
         <source src="/videos/zara-video.mp4" type="video/mp4" />
         <source src="example.webm" type="video/webm" />
