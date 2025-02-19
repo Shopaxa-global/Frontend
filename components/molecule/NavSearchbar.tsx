aed6f34d5c1c5909512db1774fceb689aa80eec8
elizabeth <leezeelola@gmail.com>
2025-02-19 23:04:46 +0100

    feat: integrate cart functionality into NavSearchbar with loading state and modal handling

--- components/molecule/NavSearchbar.tsx
+++ components/molecule/NavSearchbar.tsx
@@ -3,8 +3,11 @@ import React, { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import cancelIcon from "../../assets/images/cancel.svg";
 import searchIcon from "../../assets/images/search.svg";
+import { useCart } from "../../context/CartContext";
 import { useLocation } from "../../context/LocationContext";
-import CartModal from "../compound/CartLayout";
+import { useGetCartContent } from "../../hooks";
+import { LoadingSmall } from "../atom";
+import CartModal from "./CartLayout";
 
 const ErrorModal = ({ message = "" }) => (
   <div
@@ -38,16 +41,38 @@ type FormData = {
 
 const NavSearchbar: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [searchvalue, setSearchValue] = useState<string>("");
   const formRef = useRef<HTMLFormElement>(null);
+  const [code, setCode] = useState<string | null>(null);
   const { location } = useLocation();
+  const { setCartData, setError } = useCart();
+
+  const { cartContent, isLoading, error } = useGetCartContent(
+    code,
+    location?.country_code
+  );
+
+  // LULLBZ1
+  // L6ZVUYL
+
+  useEffect(() => {
+    if (cartContent) {
+      setCartData(cartContent);
+    }
+    if (error) {
+      setError(error);
+    }
+    setIsModalOpen(true);
+  }, [cartContent, error, setCartData, setError]);
+
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
     clearErrors,
+    watch,
   } = useForm<FormData>();
+  const codeValue = watch("code");
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -70,26 +95,40 @@ const NavSearchbar: React.FC = () => {
 
   const onSubmit = async (data: FormData) => {
     if (location) {
-      setIsModalOpen(true);
+      setCode(data.code);
     }
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
+    setCode(null);
+    reset();
+    clearErrors();
+    setError(null);
   };
 
   return (
     <>
+      {isLoading ? (
+        <div className="bg-black bg-opacity-20 fixed top-0 bottom-0 inset-x-0 cursor-none z-[5000]" />
+      ) : null}
+
       <form
         className={`h-[42px] z-[22] searchbar fixed w-full border-b border-[#000] bg-[#fff] mt-10`}
         onSubmit={handleSubmit(onSubmit)}
         ref={formRef}
       >
-        <Image
-          src={searchIcon}
-          alt=""
-          className="absolute top-[50%] lg:left-[1.2%] left-[26px] -translate-x-[50%] -translate-y-[50%]"
-        />
+        {isLoading ? (
+          <div className="absolute top-[50%] lg:left-[1.2%] left-[26px] -translate-x-[50%] -translate-y-[50%]">
+            <LoadingSmall />
+          </div>
+        ) : (
+          <Image
+            src={searchIcon}
+            alt=""
+            className="absolute top-[50%] lg:left-[1.2%] left-[26px] -translate-x-[50%] -translate-y-[50%]"
+          />
+        )}
         <input
           {...register("code", {
             required: "Code is required",
@@ -103,30 +142,28 @@ const NavSearchbar: React.FC = () => {
           placeholder="PASTE YOUR GENERATED CODE HERE"
           aria-label="Enter your 7 digit code here"
         />
-        <button
-          type="button"
-          onClick={() => {
-            reset();
-            clearErrors();
-          }}
-          className="w-full"
-        >
-          <Image
-            src={cancelIcon}
-            alt="clear search"
-            className={` ${
-              searchvalue ? "block" : "hidden"
-            } absolute top-[50%] lg:left-[98.88%] left-[calc(100vw-20px)] -translate-x-[50%] -translate-y-[50%]`}
-          />
-        </button>
+        {codeValue && (
+          <button
+            type="button"
+            onClick={() => {
+              reset();
+              clearErrors();
+            }}
+            className="w-full"
+          >
+            <Image
+              src={cancelIcon}
+              alt="clear search"
+              className={`absolute top-[50%] lg:left-[98.88%] left-[calc(100vw-20px)] -translate-x-[50%] -translate-y-[50%]`}
+            />
+          </button>
+        )}
         {errors.code && <ErrorModal message={errors.code.message} />}
       </form>
-      <CartModal
-        isOpen={isModalOpen}
-        onClose={closeModal}
-        code={searchvalue}
-        location={location?.country_code}
-      />
+
+      {code && (cartContent || error) ? (
+        <CartModal isOpen={isModalOpen} onClose={closeModal} code={code} />
+      ) : null}
     </>
   );
 };
