f71ed9fa6a095b726bc9b2ea437312c7939af545
elizabeth <leezeelola@gmail.com>
2025-02-19 22:05:01 +0100

    feat: add LoadingSmall component and update Loading styles for improved loading indicators

--- components/atom/LoadingSmall.tsx
+++ components/atom/LoadingSmall.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import styles from "./styles/loading.module.scss";
+import styles from "./styles/loadingSmall.module.scss";
 
 const Loading: React.FC = () => (
   <div
-    className="h-40 flex items-center justify-center text-[#BCBCBC] text-xs font-medium leading-[18px] capitalize"
+    className="h-[17px] flex items-center justify-center text-[#BCBCBC] text-xs font-medium leading-[18px] capitalize"
     aria-label="Loading"
   >
     <div
-      className={`${styles.lsd} flex justify-center items-center gap-2 h-full`}
+      className={`${styles.lsd} flex justify-center items-center gap-[2px] h-full`}
       aria-hidden="true"
     >
       <div aria-hidden="true"></div>
--- components/atom/index.ts
+++ components/atom/index.ts
@@ -5,6 +5,7 @@ export { default as Hambuger } from "./Hambuger";
 export { default as Heading2 } from "./Heading2";
 export { default as Links } from "./Links";
 export { default as Loading } from "./Loading";
+export { default as LoadingSmall } from "./LoadingSmall";
 export { default as PrimaryText } from "./PrimaryText";
 export { default as SecondaryHeading } from "./SecondaryHeading";
 export { default as SecondaryLink } from "./SecondaryLink";
