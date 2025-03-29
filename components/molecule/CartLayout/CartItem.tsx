073618d68879b53f3ca616cbfd6587d8e3772bf6
elizabeth <leezeelola@gmail.com>
2025-03-29 14:53:28 +0100

    feat: enhance CartLayout with improved state management, subtotal calculation, and formatting utilities

--- components/molecule/CartLayout/CartItem.tsx
+++ components/molecule/CartLayout/CartItem.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 import React, { useState } from "react";
 import closeIcon from "../../../assets/images/close-no-outline.svg";
 import convertIcon from "../../../assets/images/convert.svg";
+import { formatCurrency, formatPrice } from "../../../utils";
 
-import { CartItem as CartItemType, Conversion } from "../../../types";
+import { CartItemType, Conversion } from "../../../types";
 
 type UpdateCartItemType = {
   onQuantityChange: (name: string, newQuantity: number) => void;
@@ -18,22 +19,22 @@ const CartItem: React.FC<UpdateCartItemType> = ({
   colour,
   size,
   img,
-  maxAvailaible,
+  maxAvailable,
   productURL,
   onQuantityChange,
   conversion,
 }) => {
   const [localQuantity, setLocalQuantity] = useState(quantity);
-  const total = parseFloat((price * localQuantity).toFixed(2));
+  const total = formatPrice(price * localQuantity);
   const conVertedtotal = conversion
-    ? parseFloat((price * localQuantity * conversion.rate).toFixed(2))
+    ? formatPrice(price * localQuantity * conversion!.rate)
     : 0;
 
   const updateQuantity = (change: number) => {
     const newQuantity = localQuantity + change;
     if (newQuantity < 1) return;
-    if (newQuantity >= maxAvailaible) {
-      setLocalQuantity(maxAvailaible);
+    if (newQuantity >= maxAvailable) {
+      setLocalQuantity(maxAvailable);
     } else {
       setLocalQuantity(newQuantity);
     }
@@ -57,7 +58,7 @@ const CartItem: React.FC<UpdateCartItemType> = ({
           <Image src={closeIcon} alt="" />
         </button>
         <div className="md:w-[80%] flex flex-col justify-between md:h-[calc(100%-5rem)] h-[calc(100%-3rem)] md:block">
-          <div>
+          <div className="space-y-2">
             <div className="flex justify-between">
               <Link
                 href={productURL}
@@ -70,76 +71,80 @@ const CartItem: React.FC<UpdateCartItemType> = ({
               </Link>
             </div>
             {price ? (
-              <div className="flex gap-[6px] my-1 items-center">
-                <p>{`${total} ${conversion?.from}`}</p>
+              <div className="flex md:gap-[6px] gap-x-1 my-1 items-center">
+                <p>{`${formatCurrency(total)} ${conversion?.from}`}</p>
                 <Image
                   src={convertIcon}
                   alt="converted to"
                   className="w-auto h-auto"
                 />
-                <p>{`${conVertedtotal} ${conversion?.to}`}</p>
+                <p className="text-right md:text-left">{`${formatCurrency(
+                  conVertedtotal
+                )} ${conversion?.to}`}</p>
               </div>
             ) : null}
 
             {size && colour ? (
-              <div className="flex gap-2 items-center">
+              <div className="flex md:gap-2 gap-x-1 items-start">
                 <p>{size}</p>
                 {size ? <p>|</p> : null}
                 <p>{colour}</p>
               </div>
             ) : null}
           </div>
+
           <div className="mt-3 flex md:gap-x-4 gap-x-1 items-center">
-            <div className={`flex ${!maxAvailaible ? "opacity-50" : ""}`}>
-              <p className="border flex items-center justify-center h-8 w-8 border-black-100 md:text-[0.938rem] selection:bg-transparent">
-                {localQuantity}
-              </p>
-              <button
-                aria-label={`add another unit for ${name}`}
-                className="border-y flex items-center justify-center h-8 w-8 border-black-100 disabled:cursor-not-allowed disabled:opacity-50"
-                onClick={() => updateQuantity(1)}
-                disabled={localQuantity >= maxAvailaible}
-              >
-                <svg
-                  viewBox="0 0 13 13"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="md:w-[13px] md:h-[13px] w-[11px] h-[11px]"
-                >
-                  <path
-                    d="M6.89286 6.10714V1H6.10714V6.10714H1V6.89286H6.10714V12H6.89286V6.89286H12V6.10714H6.89286Z"
-                    fill="#0E0C22"
-                    stroke="#0E0C22"
-                    strokeWidth="0.5"
-                  />
-                </svg>
-              </button>
-              <button
-                className="border flex items-center justify-center h-8 w-8 border-black-100 disabled:cursor-not-allowed disabled:opacity-50"
-                aria-label={`remove a unit from ${name}`}
-                onClick={() => updateQuantity(-1)}
-                disabled={localQuantity <= 1}
-              >
-                <svg
-                  viewBox="0 0 16 3"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="md:w-[16px] w-[14px] h-[3px]"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M15 2H1V1H15V2Z"
-                    fill="black"
-                    stroke="black"
-                    strokeWidth="0.5"
-                  />
-                </svg>
-              </button>
-            </div>
-            {!maxAvailaible && (
+            {!maxAvailable ? (
               <div className="bg-[#BE0D0D] p-1 text-white md:text-[0.625rem] text-[0.534rem]">
                 OUT OF STOCK
+              </div>
+            ) : (
+              <div className={`flex ${!maxAvailable ? "opacity-50" : ""}`}>
+                <p className="border flex items-center justify-center h-8 w-8 border-black-100 md:text-[0.938rem] selection:bg-transparent">
+                  {localQuantity}
+                </p>
+                <button
+                  aria-label={`add another unit for ${name}`}
+                  className="border-y flex items-center justify-center h-8 w-8 border-black-100 disabled:cursor-not-allowed disabled:opacity-50"
+                  onClick={() => updateQuantity(1)}
+                  disabled={localQuantity >= maxAvailable}
+                >
+                  <svg
+                    viewBox="0 0 13 13"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="md:w-[13px] md:h-[13px] w-[11px] h-[11px]"
+                  >
+                    <path
+                      d="M6.89286 6.10714V1H6.10714V6.10714H1V6.89286H6.10714V12H6.89286V6.89286H12V6.10714H6.89286Z"
+                      fill="#0E0C22"
+                      stroke="#0E0C22"
+                      strokeWidth="0.5"
+                    />
+                  </svg>
+                </button>
+                <button
+                  className="border flex items-center justify-center h-8 w-8 border-black-100 disabled:cursor-not-allowed disabled:opacity-50"
+                  aria-label={`remove a unit from ${name}`}
+                  onClick={() => updateQuantity(-1)}
+                  disabled={localQuantity <= 1}
+                >
+                  <svg
+                    viewBox="0 0 16 3"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="md:w-[16px] w-[14px] h-[3px]"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M15 2H1V1H15V2Z"
+                      fill="black"
+                      stroke="black"
+                      strokeWidth="0.5"
+                    />
+                  </svg>
+                </button>
               </div>
             )}
           </div>
--- components/molecule/CartLayout/layout.tsx
+++ components/molecule/CartLayout/layout.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useCart } from "../../../context/CartContext";
-import { formatCreationDate } from "../../../utils";
+import { CartItemType } from "../../../types";
+import {
+  formatCreationDate,
+  formatCurrency,
+  formatPrice,
+} from "../../../utils";
 import { Error } from "../../atom";
 import CartItem from "./CartItem";
 
 const CartLayout: React.FC = () => {
   const { cartData, error } = useCart();
 
-  const [cartItems, setCartItems] = useState(cartData?.content.item || []);
-
-  useEffect(() => {
-    if (cartData?.content?.item) {
-      setCartItems(cartData.content.item);
-    }
-  }, [cartData]);
+  const [cartItems, setCartItems] = useState<CartItemType[]>([]);
+  const [subTotal, setSubTotal] = useState(0);
+  const [fee, setFee] = useState(0);
 
   const handleQuantityChange = (name: string, newQuantity: number) => {
     setCartItems((prevItems) =>
@@ -23,12 +24,26 @@ const CartLayout: React.FC = () => {
     );
   };
 
-  const subTotal = useMemo(() => {
-    return parseFloat(
-      cartItems
-        .reduce((total, item) => total + item.price * item.quantity, 0)
-        .toFixed(2)
-    );
+  useEffect(() => {
+    if (cartData?.content.item) {
+      setCartItems(cartData.content.item.map((item) => ({ ...item })));
+    }
+    return () => {
+      setCartItems([]);
+    };
+  }, [cartData]);
+
+  useEffect(() => {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total += item.price * item.quantity;
+    });
+    const _subtotal = cartData?.content.item
+      ? formatPrice(total * cartData?.content.conversion.rate!)
+      : 0.0;
+    setSubTotal(_subtotal);
+    setFee(formatPrice(Number(_subtotal) * 0.1));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cartItems]);
 
   if (error) return <Error {...error} />;
@@ -47,23 +62,26 @@ const CartLayout: React.FC = () => {
         <div className="flex items-center justify-between px-[0.625rem] py-3 md:text-xs text-[0.625rem] leading-4 md:leading-[1.125rem] font-HM-Sans text-black-100 uppercase border-b border-black-100">
           {cartData?.content.luxury ? (
             <h1 className="text-center w-full">
-              This is a {cartData?.content?.vendor?.name} ticket. Generated as
-              of {createdDate?.formattedDate}, {createdDate?.formattedTime}.
+              This is a {cartData?.content?.vendor?.name} ticket. Generated on{" "}
+              {createdDate?.formattedDate}, {createdDate?.formattedTime}.
             </h1>
           ) : (
             <>
               <h1 className="w-6/12 md:w-auto">
-                {cartData?.content.vendor.name} PRICE LIST Generated as of{" "}
+                {cartData?.content.vendor.name} PRICE LIST Generated on{" "}
                 {createdDate?.formattedDate} AT {createdDate?.formattedTime}
               </h1>
               <p className="w-4/12 self-end md:w-auto">
                 BUY Rate FOR {cartData?.content.conversion.from} ={" "}
-                {`${cartData?.content.conversion.to} ${cartData?.content.conversion.rate}`}
+                {`${cartData?.content.conversion.to} ${formatCurrency(
+                  cartData?.content.conversion.rate!
+                )}`}
               </p>
             </>
           )}
         </div>
-        <div className="md:grid xl:grid-cols-5 lg:grid-cols-4 grid-cols-2">
+
+        <div className="md:grid xl:grid-cols-5 lg:grid-cols-3 grid-cols-2">
           {cartItems.map((item) => (
             <CartItem
               key={item.name}
@@ -73,27 +91,44 @@ const CartLayout: React.FC = () => {
             />
           ))}
         </div>
+
         <div
           role="region"
           aria-live="polite"
           aria-label="Price subtotal breakdown"
           className="fixed bottom-0 w-full z-20 bg-white bg-opacity-95 flex uppercase border border-black-100"
         >
-          <div className="w-6/12 lg:w-[90%] lg:py-7 py-2 px-2 md:px-11 font-Silka text-xs leading-[1.125rem] grid justify-end text-black-100">
-            <div className="flex md:gap-7 md:justify-end justify-between font-bold">
-              <p>{cartData?.content.luxury ? "total" : "order value"}</p>
-              <p>{cartData?.content.luxury ? "pending" : "127,345 NGN"}</p>
-            </div>
-            <div className="text-[0.625rem] leading-[0.875rem] flex md:gap-2 md:justify-normal justify-between mt-1">
-              {cartData?.content.luxury ? (
-                <p>* TO BE DETERMINED</p>
-              ) : (
-                <>
-                  <p>* processing + insurance FEE =</p>
-                  <p>18,568 NGN</p>
-                </>
-              )}
-            </div>
+          <div className="w-6/12 lg:w-[90%] lg:py-7 py-2 px-2 xl:px-11 md:px-8 font-Silka text-xs leading-[1.125rem] text-black-100">
+            {cartData?.content.luxury ? (
+              <div className="flex flex-wrap md:gap-7 md:justify-end justify-between font-bold">
+                <p>Total</p>
+                <p>Pending</p>
+                <p className="w-full">* TO BE DETERMINED</p>
+              </div>
+            ) : (
+              <div className="flex md:justify-between items-center">
+                <div>
+                  <p className="hidden lg:block uppercase text-xs">
+                    PLEASE NOTE: CART EDITS DO NOT UPDATE Cart CONTENT. CLICK
+                    CHECKOUT TO CONFIRM YOUR ITEMS
+                  </p>
+                </div>
+                <div>
+                  <div className="flex md:gap-x-5 gap-x-2 gap-y-1 justify-end">
+                    <p>Order Value</p>
+                    <p>{`${formatCurrency(subTotal)} ${
+                      cartData?.content.conversion.to
+                    }`}</p>
+                  </div>
+                  <div className="w-full text-[0.5rem] md:text-[0.625rem] leading-[0.875rem] flex gap-x-2 justify-end">
+                    <p>* processing + insurance FEE</p>
+                    <p>{`${formatCurrency(fee)} ${
+                      cartData?.content.conversion.to
+                    }`}</p>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
           {cartData?.content.luxury ? (
             <button className="w-6/12 lg:w-[10%] block bg-[#212121] text-white font-HM-Sans text-xs leading-[1.125rem] font-bold lg:py-7 py-2 uppercase">
